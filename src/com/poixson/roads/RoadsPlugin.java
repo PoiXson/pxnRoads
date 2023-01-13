26e71bb269db26c76f9d2a089355bb065f56d5b6
lorenzop <lorenzo@poixson.com>
2023-01-13 07:26:33 -0500

    updated to use new xJavaPlugin class

--- src/com/poixson/roads/RoadsPlugin.java
+++ src/com/poixson/roads/RoadsPlugin.java
@@ -6,19 +6,15 @@ import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Logger;
 
-import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import org.bukkit.event.HandlerList;
-import org.bukkit.plugin.java.JavaPlugin;
 
-import com.poixson.commonmc.pxnCommonPlugin;
+import com.poixson.commonmc.tools.plugin.xJavaPlugin;
 import com.poixson.roads.commands.Commands;
-import com.poixson.tools.AppProps;
 
 
-public class RoadsPlugin extends JavaPlugin {
+public class RoadsPlugin extends xJavaPlugin {
 	public static final String LOG_PREFIX  = "[Roads] ";
 	public static final String CHAT_PREFIX = ChatColor.AQUA + "[Roads] " + ChatColor.WHITE;
 	public static final Logger log = Logger.getLogger("Minecraft");
@@ -27,8 +23,6 @@ public class RoadsPlugin extends JavaPlugin {
 	public static final int BSTATS_PLUGIN_ID = 17234;
 
 	protected static final AtomicReference<RoadsPlugin> instance = new AtomicReference<RoadsPlugin>(null);
-	protected static final AtomicReference<Metrics>     metrics  = new AtomicReference<Metrics>(null);
-	protected final AppProps props;
 
 	// listeners
 	protected final AtomicReference<Commands> commandListener = new AtomicReference<Commands>(null);
@@ -38,18 +32,14 @@ public class RoadsPlugin extends JavaPlugin {
 
 
 	public RoadsPlugin() {
-		super();
-		try {
-			this.props = AppProps.LoadFromClassRef(RoadsPlugin.class);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
+		super(RoadsPlugin.class);
 	}
 
 
 
 	@Override
 	public void onEnable() {
+		super.onEnable();
 		if (!instance.compareAndSet(null, this))
 			throw new RuntimeException("Plugin instance already enabled?");
 		// commands listener
@@ -60,34 +50,17 @@ public class RoadsPlugin extends JavaPlugin {
 				previous.unregister();
 			listener.register();
 		}
-		// bStats
-		System.setProperty("bstats.relocatecheck","false");
-		metrics.set(new Metrics(this, BSTATS_PLUGIN_ID));
-		// update checker
-		pxnCommonPlugin.GetPlugin()
-			.getUpdateCheckManager()
-				.addPlugin(this, SPIGOT_PLUGIN_ID, this.getPluginVersion());
 	}
 
 	@Override
 	public void onDisable() {
-		// update checker
-		pxnCommonPlugin.GetPlugin()
-			.getUpdateCheckManager()
-				.removePlugin(SPIGOT_PLUGIN_ID);
+		super.onDisable();
 		// commands listener
 		{
 			final Commands listener = this.commandListener.getAndSet(null);
 			if (listener != null)
 				listener.unregister();
 		}
-		// stop schedulers
-		try {
-			Bukkit.getScheduler()
-				.cancelTasks(this);
-		} catch (Exception ignore) {}
-		// stop listeners
-		HandlerList.unregisterAll(this);
 		if (!instance.compareAndSet(this, null))
 			throw new RuntimeException("Disable wrong instance of plugin?");
 	}
@@ -116,8 +89,17 @@ public class RoadsPlugin extends JavaPlugin {
 
 
 
-	public String getPluginVersion() {
-		return this.props.version;
+	// -------------------------------------------------------------------------------
+
+
+
+	@Override
+	protected int getSpigotPluginID() {
+		return SPIGOT_PLUGIN_ID;
+	}
+	@Override
+	protected int getBStatsID() {
+		return BSTATS_PLUGIN_ID;
 	}
 
 
