d847ff4345b2eda8098521fe22c0f214b5fe69b4
lorenzop <lorenzo@poixson.com>
2023-01-13 03:19:00 -0500

    added update checker

--- src/com/poixson/roads/RoadsPlugin.java
+++ src/com/poixson/roads/RoadsPlugin.java
@@ -1,5 +1,6 @@
 package com.poixson.roads;
 
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.atomic.AtomicReference;
@@ -12,6 +13,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.poixson.commonbukkit.pxnCommonPlugin;
 import com.poixson.roads.commands.Commands;
 import com.poixson.tools.AppProps;
 
@@ -20,6 +22,8 @@ public class RoadsPlugin extends JavaPlugin {
 	public static final String LOG_PREFIX  = "[Roads] ";
 	public static final String CHAT_PREFIX = ChatColor.AQUA + "[Roads] " + ChatColor.WHITE;
 	public static final Logger log = Logger.getLogger("Minecraft");
+//TODO
+	public static final int SPIGOT_PLUGIN_ID = 0;
 	public static final int BSTATS_PLUGIN_ID = 17234;
 
 	protected static final AtomicReference<RoadsPlugin> instance = new AtomicReference<RoadsPlugin>(null);
@@ -59,10 +63,18 @@ public class RoadsPlugin extends JavaPlugin {
 		// bStats
 		System.setProperty("bstats.relocatecheck","false");
 		metrics.set(new Metrics(this, BSTATS_PLUGIN_ID));
+		// update checker
+		pxnCommonPlugin.GetPlugin()
+			.getUpdateCheckManager()
+				.addPlugin(this, SPIGOT_PLUGIN_ID, this.getPluginVersion());
 	}
 
 	@Override
 	public void onDisable() {
+		// update checker
+		pxnCommonPlugin.GetPlugin()
+			.getUpdateCheckManager()
+				.removePlugin(SPIGOT_PLUGIN_ID);
 		// commands listener
 		{
 			final Commands listener = this.commandListener.getAndSet(null);
@@ -100,6 +112,12 @@ public class RoadsPlugin extends JavaPlugin {
 	public boolean stopFollower(final PlayerFollower follower) {
 		follower.stop();
 		return this.followers.remove(follower);
+	}
+
+
+
+	public String getPluginVersion() {
+		return this.props.version;
 	}
 
 
